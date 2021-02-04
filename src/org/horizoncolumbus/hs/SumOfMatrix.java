7e59d4dfe55c47d153579af0c17099d14e4e3f74
Kwabena <student.k.nkansah-boateng@horizoncolumbus.org>
2021-02-04 08:46:40 -0500

    Done

--- src/org/horizoncolumbus/hs/SumOfMatrix.java
+++ src/org/horizoncolumbus/hs/SumOfMatrix.java
@@ -6,11 +6,11 @@ public class SumOfMatrix {
         int matrix[][] = {{0,1,1},{9,2,4},{9,0,4},{1,2,5}};
 
         rows = matrix[0].length;
-        cols = matrix.length;
+        cols = matrix[0].length;
 
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < rows; i++){
             sumCol = 0;
-            for(int j = 0; j < sumCol; j++){
+            for(int j = 0; j < cols; j++){
                 sumCol = sumCol + matrix[j][i];
                 System.out.print(matrix[i][j] + "\t");
             }
@@ -22,7 +22,7 @@ public class SumOfMatrix {
             for(int j = 0; j < 3; j++) {
                 sumRow = sumRow + matrix[i][j];
             }
-            System.out.println("Sum of " + " row " + (i+1)  + " = " + (sumRow));
+            System.out.println("Sum of" + " row " + (i+1)  + " = " + (sumRow));
         }
     }
 }
