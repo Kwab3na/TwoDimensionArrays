29e4728f5904db7f156fcabffdc465d50dbd16a4
Kwabena <student.k.nkansah-boateng@horizoncolumbus.org>
2021-02-11 08:47:13 -0500

    Done with both codes

--- src/org/horizoncolumbus/hs/Main.java
+++ src/org/horizoncolumbus/hs/Main.java
@@ -3,13 +3,25 @@ package org.horizoncolumbus.hs;
 public class Main {
 
     public static void main(String[] args) {
-	int arr[][] = { {8,7,9},{3,7,5},{7,4,4} };
+		int[][] arr = new int[3][3];
 
-	for(int i = 0; i < 3; i++){
-	    for(int j = 0; j < 3; j++){
-	        System.out.println(arr[i][j]);
-        }
-    }
-	    System.out.println("\n");
-    }
+		arr[0][0] = 2;
+		arr[0][1] = 7;
+		arr[0][2] = 9;
+
+		arr[1][0] = 3;
+		arr[1][1] = 6;
+		arr[1][2] = 1;
+
+		arr[2][0] = 7;
+		arr[2][1] = 4;
+		arr[2][2] = 2;
+
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++){
+				System.out.print(arr[j][i] + "\t");
+			}
+			System.out.println("\n");
+		}
+	}
 }
--- src/org/horizoncolumbus/hs/SumOfMatrix.java
+++ src/org/horizoncolumbus/hs/SumOfMatrix.java
@@ -2,27 +2,15 @@ package org.horizoncolumbus.hs;
 
 public class SumOfMatrix {
     public static void main(String[] args) {
-        int rows, cols, sumRow, sumCol;
         int matrix[][] = {{0,1,1},{9,2,4},{9,0,4},{1,2,5}};
 
-        rows = matrix[0].length;
-        cols = matrix[0].length;
-
-        for(int i = 0; i < rows; i++){
-            sumCol = 0;
-            for(int j = 0; j < cols; j++){
-                sumCol = sumCol + matrix[j][i];
-                System.out.print(matrix[i][j] + "\t");
-            }
-            System.out.println("\n");
-        }
-
         for(int i = 0; i < 4; i++){
-            sumRow = 0;
-            for(int j = 0; j < 3; j++) {
-                sumRow = sumRow + matrix[i][j];
+            int sum = 0;
+            for(int j = 0; j < 3; j++){
+                sum = sum + matrix[i][j];
+                System.out.print(matrix[i][j] + " ");
             }
-            System.out.println("Sum of" + " row " + (i+1)  + " = " + (sumRow));
+            System.out.println(" = " + sum);
         }
     }
 }
